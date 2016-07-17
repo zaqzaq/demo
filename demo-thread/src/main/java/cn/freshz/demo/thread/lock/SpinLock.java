28f67a6712932b5e9a8c4282f7f3729824b49de7
yjzhang <382566697@qq.com>
2016-07-17 12:48:35 +0800

    Update SpinLock.java

--- demo-thread/src/main/java/cn/freshz/demo/thread/lock/SpinLock.java
+++ demo-thread/src/main/java/cn/freshz/demo/thread/lock/SpinLock.java
@@ -7,12 +7,14 @@ public class SpinLock {
 
   public void lock(){
     Thread current = Thread.currentThread();
+    // 如果锁未被占用，则设置当前线程为锁的拥有者
     while(!sign .compareAndSet(null, current)){
     }
   }
 
   public void unlock (){
     Thread current = Thread.currentThread();
+     // 只有锁的拥有者才能释放锁
     sign .compareAndSet(current, null);
   }
 }
