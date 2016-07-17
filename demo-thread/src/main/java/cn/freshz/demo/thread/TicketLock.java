d995bec1092755a50bb01a2f73c9c43c400fce7d
yjzhang <382566697@qq.com>
2016-07-17 12:39:19 +0800

    Update TicketLock.java

--- demo-thread/src/main/java/cn/freshz/demo/thread/TicketLock.java
+++ demo-thread/src/main/java/cn/freshz/demo/thread/TicketLock.java
@@ -3,6 +3,8 @@ package com.alipay.titan.dcc.dal.entity;
 import java.util.concurrent.atomic.AtomicInteger;
 /**
 *Ticket锁主要解决的是访问顺序的问题，主要的问题是在多核cpu上  ,顺序 公平  锁
+* 
+* 每次都要查询一个serviceNum 服务号，影响性能（必须要到主内存读取，并阻止其他cpu修改）。
 */
 public class TicketLock {
     private AtomicInteger                     serviceNum = new AtomicInteger();
