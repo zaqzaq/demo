e995db9dbd4dd6fde5f0c095b8a0ee24680adaf0
yjzhang <382566697@qq.com>
2016-07-17 11:43:48 +0800

    Create SpinLock.java

---
+++ demo-thread/src/main/java/cn/freshz/demo/thread/lock/SpinLock.java
@@ -0,0 +1,18 @@
+/**
+*自旋锁
+*/
+public class SpinLock {
+
+  private AtomicReference<Thread> sign =new AtomicReference<>();
+
+  public void lock(){
+    Thread current = Thread.currentThread();
+    while(!sign .compareAndSet(null, current)){
+    }
+  }
+
+  public void unlock (){
+    Thread current = Thread.currentThread();
+    sign .compareAndSet(current, null);
+  }
+}
