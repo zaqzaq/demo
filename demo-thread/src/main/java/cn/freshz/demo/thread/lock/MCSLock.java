0f0507f124000b29abf312a44a1e26fb63925cc8
yjzhang <382566697@qq.com>
2016-07-17 13:00:44 +0800

    Update MCSLock.java

--- demo-thread/src/main/java/cn/freshz/demo/thread/lock/MCSLock.java
+++ demo-thread/src/main/java/cn/freshz/demo/thread/lock/MCSLock.java
@@ -1,9 +1,12 @@
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
-
+/**
+ * MCS Spinlock 是一种基于链表的可扩展、高性能、公平的自旋锁，申请线程只在本地变量上自旋，
+ * 直接前驱负责通知其结束自旋，从而极大地减少了不必要的处理器缓存同步的次数，降低了总线和内存的开销。
+ */
 public class MCSLock {
     public static class MCSNode {
         volatile MCSNode next;
-        volatile boolean isLocked = true;
+        volatile boolean isLocked = true;// 默认是在等待锁
     }
 
     private static final ThreadLocal<MCSNode>                          NODE    = new ThreadLocal<MCSNode>();
@@ -27,6 +30,7 @@ public class MCSLock {
     public void unlock() {
         MCSNode currentNode = NODE.get();
         if (currentNode.next == null) {
+         
             if (UPDATER.compareAndSet(this, currentNode, null)) {
 
             } else {
